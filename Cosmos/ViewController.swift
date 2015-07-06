7b4ddfd7cf7d4215a99aaffeacf6bc6a54ccf5f0
-- C4 -- <info@c4ios.com>
2015-07-05 18:53:10 -0700

    Initial Commit

---
+++ Cosmos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cosmos
+//
+//  Created by travis on 2015-07-05.
+//  Copyright (c) 2015 C4. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
