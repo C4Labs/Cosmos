1c44f0f380bde3c4f572e9a56d7a0445487e6961
travis <travis@c4ios.com>
2015-10-19 17:11:13 -0700

    Renames ViewController to WorkSpace

--- Cosmos/WorkSpace.swift
+++ Cosmos/WorkSpace.swift
@@ -25,7 +25,7 @@ let cosmosprpl = C4Color(red:0.565, green: 0.075, blue: 0.996, alpha: 1.0)
 let cosmosblue = C4Color(red: 0.094, green: 0.271, blue: 1.0, alpha: 1.0)
 let cosmosbkgd = C4Color(red: 0.078, green: 0.118, blue: 0.306, alpha: 1.0)
 
-class ViewController: C4CanvasController {
+class WorkSpace: C4CanvasController {
     override func setup() {
         //set the background color of the main app
         canvas.backgroundColor = cosmosbkgd
