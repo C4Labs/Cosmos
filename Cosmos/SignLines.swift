8b935f22d497c60f9ea9165a1ce49f4d0b2e0378
travis <travis@c4ios.com>
2015-10-20 14:06:23 -0700

    Adds deleted initializer to SignLines

--- Cosmos/SignLines.swift
+++ Cosmos/SignLines.swift
@@ -24,4 +24,8 @@ public class SignLines : InfiniteScrollView {
     public override init(frame: CGRect) {
         super.init(frame: frame)
     }
+
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
 }
