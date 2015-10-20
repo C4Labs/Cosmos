f26224f711f02bb616a1d38e4c54658743df696d
travis <travis@c4ios.com>
2015-10-19 18:03:45 -0700

    Restructures and comments MenuRings

--- Cosmos/MenuRings.swift
+++ Cosmos/MenuRings.swift
@@ -25,11 +25,16 @@ public class MenuRings : C4CanvasController {
     //MARK: Properties
     var thickRing : C4Circle!
     var thickRingFrames : [C4Rect]!
+    var thickRingOut : C4ViewAnimation?
+    var thickRingIn : C4ViewAnimation?
 
     var thinRings : [C4Circle]!
     var thinRingFrames : [C4Rect]!
-    
+    var thinRingsOut : C4ViewAnimationSequence?
+
     var dashedRings : [C4Circle]!
+    var revealDashedRings : C4ViewAnimation?
+    var hideDashedRings : C4ViewAnimation?
     
     var menuDividingLines : [C4Line]!
     
@@ -37,7 +42,9 @@ public class MenuRings : C4CanvasController {
     
     
     public override func setup() {
+        //clear the canvas
         canvas.backgroundColor = clear
+        //make the canvas small (helps isolate its interactivity to a small portion of the main app's screen)
         canvas.frame = C4Rect(0,0,80,80)
         createRingsLines()
         createRingsLinesAnimations()
@@ -45,7 +52,6 @@ public class MenuRings : C4CanvasController {
     
     //MARK: -
     //MARK: Rings and Lines
-    
     func createRingsLines() {
         createThickRing()
         createThinRings()
@@ -60,11 +66,15 @@ public class MenuRings : C4CanvasController {
         createDashedRingAnimations()
     }
     
+    //creates the thick ring
     func createThickRing() {
+        //create 2 shapes
         let inner = C4Circle(center: canvas.center, radius: 14)
         let outer = C4Circle(center: canvas.center, radius: 225)
+        //store the frames of each position
         thickRingFrames = [inner.frame,outer.frame]
         
+        //style the inner ring and use it to create the `thickRing` object
         inner.fillColor = clear
         inner.lineWidth = 3
         inner.strokeColor = cosmosblue
@@ -74,15 +84,15 @@ public class MenuRings : C4CanvasController {
         canvas.add(thickRing)
     }
     
-    var thickRingOut : C4ViewAnimation?
-    var thickRingIn : C4ViewAnimation?
-    
+    //creates the in / out animations for the thick ring
     func createThickRingAnimations() {
+        //animates the frame of the ring to its open position
         thickRingOut = C4ViewAnimation(duration: 0.5) {
             self.thickRing?.frame = self.thickRingFrames[1]
         }
         thickRingOut?.curve = .EaseOut
         
+        //animates the frame of the ring to its closed position
         thickRingIn = C4ViewAnimation(duration: 0.5) {
             self.thickRing?.frame = self.thickRingFrames[0]
         }
@@ -90,6 +100,7 @@ public class MenuRings : C4CanvasController {
     }
     
     func createThinRings() {
+        //creates the closed position of each ring
         thinRings = [C4Circle]()
         thinRings.append(C4Circle(center: canvas.center, radius: 8))
         thinRings.append(C4Circle(center: canvas.center, radius: 56))
@@ -98,6 +109,7 @@ public class MenuRings : C4CanvasController {
         thinRings.append(C4Circle(center: canvas.center, radius: 102))
         thinRings.append(C4Circle(center: canvas.center, radius: 156))
         
+        //iterate through all the current thin rings to grab their frames
         thinRingFrames = [C4Rect]()
         for i in 0..<self.thinRings.count {
             let ring = self.thinRings[i]
@@ -111,20 +123,23 @@ public class MenuRings : C4CanvasController {
             self.thinRingFrames.append(ring.frame)
         }
         
+        //add all the rings to the canvas
         for ring in thinRings {
             canvas.add(ring)
         }
     }
     
-    var thinRingsOut : C4ViewAnimationSequence?
-    
     func createThinRingsOutAnimations() {
+        //create an array to store the animations we'll create
         var animationArray = [C4ViewAnimation]()
+        //for each of the rings (except the last one)
         for i in 0..<self.thinRings.count-1 {
+            //create an aninmation whose duration is slightly longer than the previous one
             let anim = C4ViewAnimation(duration: 0.075 + Double(i) * 0.01) {
                 let circle = self.thinRings[i]
-                
+                //for each animation greatre than the first
                 if (i > 0) {
+                    //animate the opacity of the ring to 1.0
                     C4ViewAnimation(duration: 0.0375) {
                         circle.opacity = 1.0
                         }.animate()
@@ -135,17 +150,23 @@ public class MenuRings : C4CanvasController {
             anim.curve = .EaseOut
             animationArray.append(anim)
         }
+        //create an animation sequence for animating the thin rings out
         thinRingsOut = C4ViewAnimationSequence(animations: animationArray)
     }
     
     var thinRingsIn : C4ViewAnimationSequence?
     
     func createThinRingsInAnimations() {
+        //create an array to store the animations we'll create
         var animationArray = [C4ViewAnimation]()
+        //for each of the rings (except the first one)
         for i in 1...self.thinRings.count {
+            //create an aninmation whose duration is slightly longer than the previous one
             let anim = C4ViewAnimation(duration: 0.075 + Double(i) * 0.01, animations: { () -> Void in
+                //grab the last ring and work towards the first
                 let circle = self.thinRings[self.thinRings.count - i]
                 if self.thinRings.count - i > 0 {
+                    //for each ring that isn't the first, animate its opacity to 0.0
                     C4ViewAnimation(duration: 0.0375) {
                         circle.opacity = 0.0
                         }.animate()
@@ -173,11 +194,14 @@ public class MenuRings : C4CanvasController {
     }
     
     func createShortDashedRing() {
+        //creates a new ring and styles it
         let shortDashedRing = C4Circle(center: canvas.center, radius: 82+2)
+        //creates a pattern, with 4 dashes and 1 wide gap
         let pattern = [1.465,1.465,1.465,1.465,1.465,1.465,1.465,1.465*3.0] as [NSNumber]
         shortDashedRing.lineDashPattern = pattern
         shortDashedRing.strokeEnd = 0.995
         
+        //adjusts the angle of the shape's rotation to make the gap appear at 0 position
         let angle = degToRad(-1.5)
         let rotation = C4Transform.makeRotation(angle)
         shortDashedRing.transform = rotation
@@ -187,18 +211,21 @@ public class MenuRings : C4CanvasController {
     }
     
     func createLongDashedRing() {
+        //creates a new ring and styles it
         let longDashedRing = C4Circle(center: canvas.center, radius: 82+2)
-        
         longDashedRing.lineWidth = 0.0
-        
+
+        //creates a pattern, with 1 dash and 1 very wide gap
         let pattern = [1.465,1.465*9.0] as [NSNumber]
         longDashedRing.lineDashPattern = pattern
         longDashedRing.strokeEnd = 0.995
         
+        //adjusts the angle of the shape's rotation to make the dash appear at 0 position
         let angle = degToRad(0.5)
         let rotation = C4Transform.makeRotation(angle)
         longDashedRing.transform = rotation
         
+        //adds a mask to the long dashed ring so that it looks like it lines up with the inner radius of the short ring
         let mask = C4Circle(center: C4Point(longDashedRing.width/2.0,longDashedRing.height/2.0), radius: 82+4)
         mask.fillColor = clear
         mask.lineWidth = 8
@@ -207,10 +234,8 @@ public class MenuRings : C4CanvasController {
         dashedRings.append(longDashedRing)
     }
     
-    var revealDashedRings : C4ViewAnimation?
-    var hideDashedRings : C4ViewAnimation?
-    
     func createDashedRingAnimations() {
+        //instead of changing the opacities, we animate the line width for a nicer effect
         revealDashedRings = C4ViewAnimation(duration: 0.25) {
             self.dashedRings[0].lineWidth = 4
             self.dashedRings[1].lineWidth = 12
@@ -226,6 +251,7 @@ public class MenuRings : C4CanvasController {
     
     func createMenuDividingLines() {
         menuDividingLines = [C4Line]()
+        //adds 12 lines and rotates them into position
         for i in 0...11 {
             let line = C4Line((C4Point(),C4Point(54,0)))
             line.anchorPoint = C4Point(-1.88888,0)
@@ -240,10 +266,16 @@ public class MenuRings : C4CanvasController {
         }
     }
     
+    //instead of having two separate methods, we use a target value for the strokeEnd of the lines
+    //e.g. hide = 0.0
+    //we do this because the logic is random, and identical in either direction
     func revealHideDividingLines(target: Double) {
+        //create a list of indices that need to be animated
         var indices = [0,1,2,3,4,5,6,7,8,9,10,11]
-        
+
+        //instead of changing the opacities, we animate the line width for a nicer effect
         for i in 0...11 {
+            //create a delay, and grab a line based on random selection from the local indices[]
             delay(0.05*Double(i)) {
                 let randomIndex = random(below: indices.count)
                 let index = indices[randomIndex]
@@ -251,7 +283,7 @@ public class MenuRings : C4CanvasController {
                 C4ViewAnimation(duration: 0.1) {
                     self.menuDividingLines[index].strokeEnd = target
                     }.animate()
-                
+                //removes the current index from the local list so we can continue grabbing the remaining shapes
                 indices.removeAtIndex(randomIndex)
             }
         }
